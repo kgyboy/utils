d096d26892a590c1a3e6df8a848c0b067de12a63
yangsibiao <yangsibiao@baiwang.com>
2019-10-15 16:34:50 +0800

    regex substr

--- src/main/java/com/kgyboy/utils/StringUtils.java
+++ src/main/java/com/kgyboy/utils/StringUtils.java
@@ -1,5 +1,10 @@
 package com.kgyboy.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * author yang
  * time 2019-01-25
@@ -20,7 +25,7 @@ public class StringUtils {
      * 判断是否为空
      */
     public static boolean isBlank(String str) {
-        if (null == str || str.replace(" ","").isEmpty()) {
+        if (null == str || str.replace(" ", "").isEmpty()) {
             return true;
         }
 
@@ -28,6 +33,20 @@ public class StringUtils {
 //            return true;
 //        }
         return false;
+    }
+
+    /**
+     * 正则截取字符串
+     */
+    public static List<String> regexSubstr(String str, char start, char end) {
+        List<String> list = new ArrayList<>();
+        String regex = "\\" + start + "[^\\" + end + "]+\\" + end;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            list.add(matcher.group().replace(start + "", "").replace(end + "", ""));
+        }
+        return list;
     }
 
 }
